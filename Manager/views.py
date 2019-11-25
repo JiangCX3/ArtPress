2786a43aa369866667b38ffe032c69db31f3d784
JiangCX3 <lazyfox158@outlook.com>
2019-11-25 20:58:32 +0800

    调整细节

--- Manager/views.py
+++ Manager/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def ap_manager(request):
     return HttpResponseRedirect("/ap-manager/home/")
 
-def home(request):
-    return render(request, 'Manager/home.html')
 
+def home(request):
+    print(request.user)
+    return render(request, 'Manager/home.html', {
+        "user": request.user,
+    })
