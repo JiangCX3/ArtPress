e645976acd2748500bed0343565d8954b2a163be
JiangCX3 <lazyfox158@outlook.com>
2019-12-05 18:46:51 +0800

    基本资料更改页完成

--- Users/models.py
+++ Users/models.py
@@ -5,8 +5,11 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
+    nickname = models.CharField('Nick Name', max_length=30, blank=True)
     avatar_filename = models.CharField('Avatar filename', max_length=128, blank=True)
     mod_date = models.DateTimeField('Last modified', auto_now=True)
+    bio = models.CharField('Bio', max_length=1024, blank=True)
+    url = models.CharField('URL', max_length=128, blank=True)
 
     class Meta:
         verbose_name = 'User Profile'
--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth import logout, models, authenticate, login
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+
 # Create your views here.
+def change_password(request):
+    return
