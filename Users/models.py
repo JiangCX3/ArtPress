482670492d193b81d6cfecb7aa2038c4decf6fb0
JiangCX3 <lazyfox158@outlook.com>
2019-12-05 21:51:41 +0800

    完善me页面表单内容

--- Users/models.py
+++ Users/models.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
-    nickname = models.CharField('Nick Name', max_length=30, blank=True)
+    # nickname = models.CharField('Nick Name', max_length=30, blank=True)
     avatar_filename = models.CharField('Avatar filename', max_length=128, blank=True)
     mod_date = models.DateTimeField('Last modified', auto_now=True)
-    bio = models.CharField('Bio', max_length=1024, blank=True)
+    bio = models.TextField('Bio', blank=True)
     url = models.CharField('URL', max_length=128, blank=True)
 
     class Meta:
