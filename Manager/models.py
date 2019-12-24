d7ecf91890dcf1a23ff036809aabbbc208199efe
JiangCX3 <lazyfox158@outlook.com>
2019-12-24 22:32:51 +0800

    Medias model done.

--- Manager/models.py
+++ Manager/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Medias(models.Model):
-    file
-    user_id
-    exif
+    file = models.CharField('Manager filepath', max_length=128)
+    user_id = models.IntegerField('Author ID', null=True)
+    exif = models.TextField('Exif Encoded Data', null=True)
