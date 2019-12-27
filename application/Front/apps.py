d833b8c173605a71f7e3c668f68ad9ced9e76434
JiangCX3 <lazyfox158@outlook.com>
2019-12-27 22:29:50 +0800

    修复代码库

--- application/Front/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-from django.shortcuts import render
-
-
-class FrontConfig(AppConfig):
-    name = 'Application.Front'
--- application/Verify/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from Application.Verify import models
-
-# Register your models here.
-admin.site.register(models.VerifyCode)
